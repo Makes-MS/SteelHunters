d58afede85f2699a52a7a0db5c556a5f966b3efd
Maxym <MakesMS24@gmail.com>
2019-11-17 09:35:58 +0200

    First commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/steelhunters/admin/');
+define('HTTP_CATALOG', 'http://localhost/steelhunters/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/steelhunters/admin/');
+define('HTTPS_CATALOG', 'http://localhost/steelhunters/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/steelhunters/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/steelhunters/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/steelhunters/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/steelhunters/admin/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/steelhunters/admin/view/template/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/steelhunters/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/steelhunters/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/steelhunters/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/steelhunters/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/steelhunters/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/steelhunters/system/storage/upload/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/steelhunters/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'steelhunters');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
